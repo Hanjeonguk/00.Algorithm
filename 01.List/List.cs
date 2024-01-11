61f78ed4c68631ebe489cdac42526ac0c199b6c3
Hanjeonguk <wjddnr1358@naver.com>
2024-01-11 16:53:53 +0900

    초기 커밋.

---
+++ 01.List/List.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure
+{
+    //List 생성 과정
+    public class List<T>//List 클래스 생성, 자료형 일반화
+    {
+        private const int DefaultCapacity = 4;
+        private T[] items;
+        private int count;
+
+        public List() 
+        {
+            items = new T[DefaultCapacity];
+            count = 0;
+        }
+
+        public List(int capacity)
+        {
+            items =new T[capacity];
+             count = 0;
+        }
+
+        public int Capacity { get { return items.Length; } }
+        public int Count { get { return count; } }
+
+        public bool IsFull { get { return count == items.Length; } }//추가 방식
+
+        public T this[int index] 
+        { 
+            get 
+            {
+                return items[index];
+            }
+            set 
+            {
+                items[index] = value;
+            }
+        }
+        public void Add(T item)
+        {
+            if (count== items.Length)// 배열이 다 채워졌을때
+            if (IsFull)
+                Grow();
+            items[count++] = item;
+
+        }
+        public void Insert(int index, T item)
+        {
+            //예외처리 필요 : 크기를 벗어나게 중간에 끼워넣는 것이 불가능
+            if (index < 0 || index > count)
+           //     Console.WriteLine("Error 불가능하다.");
+            throw new ArgumentOutOfRangeException("index");//추가 방식
+
+            if (IsFull)
+                Grow();
+
+            Array.Copy(items, index, items, index + 1, count - index);
+
+            items[index] = item;
+            count++;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index < 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            //예외처리 필요 : 크기를 벗어나게 중간에 끼워넣는 것이 불가능
+            if (index < 0 || index >= count)
+              //  Console.WriteLine("Error 불가능하다.");
+            throw new ArgumentOutOfRangeException("index");//추가 방식
+
+            count--;
+            Array.Copy(items, index + 1, items, index, count - index);
+        }
+
+        public int IndexOf(T item)
+        {
+            for(int i=0; i <  count; i++)
+            {
+                 if (item.Equals(items[i]))
+                {
+                    return i;                    
+                }
+            }
+            return -1;
+        }
+
+        private void Grow()
+        {
+            //2. 새로운 더 큰 배열 생성
+            T[] newItems = new T[items.Length*2];
+
+            //3. 새로운 배열에 기존의 데이터 복사
+            Array.Copy(items, newItems, items.Length);
+
+            //4. 기본 배열 대신 새로운 배열을 사용
+            items = newItems;
+
+            //5. 빈 공간에 데이터 추가
+
+        }
+    }
+}
+
---
+++ Task24.01.11/Program.cs
@@ -0,0 +1,81 @@
+﻿// 사용자에게 숫자를 입력 받아서
+// 0부터 숫자까지 가지는 리스트 만들기
+// 0요소를 제거
+// 리스트가 가지는 모든 요소들을 출력해주는 반복분 작성
+namespace Task24._01._11_1
+{
+    public class Program
+    {
+        static void Main1(string[] args)
+        {
+
+            List<int> list = new List<int>();
+
+            Console.Write("숫자를 입력하세요 : ");
+            int input = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i <= input; i++)
+            {
+
+                list.Add(i);
+
+            }
+            list.Remove(0);
+
+            for (int i = 0; i <= list.Count; i++)
+            {
+                Console.Write($"{list[i]}  ");
+            }
+        }
+    }
+}
+
+// 사용자의 입력을 받아서 없는 데이터면 추가, 있던 데이터면 삭제하는 보관함
+// ex) 1,6,7,8,3 들고 있던 상황이면
+// 2입력하면 1,6,7,8,3,2
+// 7 입력하면 1,6 8,3
+namespace Task24._01._11__2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(3);
+
+            Console.Write("숫자를 입력하세요 : ");
+            int input = int.Parse(Console.ReadLine());
+            int num = list.Count;
+
+            bool b = list.IndexOf(input) != -1;
+
+
+            if (b == true)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    if (list[i] == input)
+                        list.RemoveAt(i);    
+                }
+            }
+            else
+            {
+                list.Add(input);
+            }
+
+            for (int j = 0; j < list.Count; j++)
+            {
+                Console.Write($"{list[j]} ");
+            }
+        }
+    }
+}
+// 인벤토리 구현(아이템 수집, 아이템 버리기)
+
+// 브랜치 Homework 만들어서 저장
+
